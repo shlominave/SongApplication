12c1d4c82c5a70487f081ed88d3ba166f417a4a6
shlominave <s1170@motzkine.ort.org.il>
2020-12-09 23:07:00 +0200

    second commit

--- app/src/main/java/com/example/songapplication/Country.java
+++ app/src/main/java/com/example/songapplication/Country.java
@@ -1,6 +1,46 @@
 package com.example.songapplication;
 
 public class Country {
-    public String countryname;
-    private int population;
+    private String countryname;
+    private String countryflag;
+    private long population;
+
+    public Country(String countryname, String countryflag, long population) {
+        this.countryname = countryname;
+        this.countryflag = countryflag;
+        this.population = population;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getCountryflag() {
+        return countryflag;
+    }
+
+    public void setCountryflag(String countryflag) {
+        this.countryflag = countryflag;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(long population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "countryname='" + countryname + '\'' +
+                ", countryflag='" + countryflag + '\'' +
+                ", population=" + population +
+                '}';
+    }
 }
--- app/src/main/java/com/example/songapplication/MainActivity.java
+++ app/src/main/java/com/example/songapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.songapplication;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,52 +22,101 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 ListView ListView1;
-ArrayList<Song>song=new ArrayList<Song>();
+//ArrayList<Song>song=new ArrayList<Song>();
+ArrayList<Country> countrylist =new  ArrayList<Country>();
+YourAdapter ad;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView1=findViewById(R.id.LV1);
-        getsongdata();
-      //  SongAdapter sa=new SongAdapter();
-        SongAdapter.OurSongAdapter sa(this,R.layout.Song,song)
+        getcountrydata();
+        ad=new YourAdapter(this,R.layout.countrylayout,countrylist);
+        ListView1=(ListView)findViewById(R.id.LV1);
+
+        ListView1.setAdapter(ad);
+        ListView1.setOnItemClickListener(Itemlistener);
+
+//        getsongdata();
+//      //  SongAdapter sa=new SongAdapter();
+//        SongAdapter.OurSongAdapter sa(this,R.layout.Song,song)
 
 
     }
-    public void getsongdata()
+    private AdapterView.OnItemClickListener Itemlistener=new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(MainActivity.this, countrylist.get(position).toString(), Toast.LENGTH_SHORT).show();
+        }
+    };
+    public void getcountrydata()
     {
-        Song s=new Song("singer1","song 1","pic1",2);
-        song.add(s);
-         s=new Song("singer1","song 2","pic1",2);
-        song.add(s);
-         s=new Song("singer1","song 3","pic1",2);
-        song.add(s);
-         s=new Song("singer1","song 4","pic1",2);
-        song.add(s);
-        s=new Song("singer1","song 5","pic1",2);
-        song.add(s);
+        Country c=new Country("serbia","serbia.png",20000000);
+      countrylist.add(c);
+        c=new Country("belarus","belarus.png",9400000);
+        countrylist.add(c);
+        c=new Country("spain","spain.png",40000000);
+        countrylist.add(c);
+        c=new Country("unitedkingdom","unitedkingdom.png",64000000);
+        countrylist.add(c);
+
     }
-    public class SongAdapter extends ArrayList<Song> {
+    public class YourAdapter extends ArrayAdapter<Country> {
+       private Context c;
+       private int resource;
+       private ArrayList<Country> objects;
 
+        public YourAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Country> objects) {
+            super(context, resource, objects);
+            this.c=c;
+            this.resource=resource;
+            this.objects=objects;
+        }
 
-        public class OurSongAdapter extends ArrayList<Song>{
+        @Override
+        public int getCount() {
+            return this.objects.size();
+        }
 
-
-                Context c;
-                int resource;
-                List<Song> objects;
-                public OurSongAdapter(Context c1, int resource, List<Song> objects)
-                {
-                super(c1,resource,objects);
-                this.c = c1;
-                this.resource = resource;
-                this.objects = objects;
-            }
-
-
-
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+         View v=null;
+            LayoutInflater li=(LayoutInflater)this.c.getSystemService(c.LAYOUT_INFLATER_SERVICE);
+         v=li.inflate(this.resource,null);
+         Country c=this.objects.get(position);
+            TextView countrynametv =findViewById(R.id.countrynametv);
+            TextView populationtv =findViewById(R.id.populationtv);
+            ImageView iv=findViewById(R.id.imageView1);
+            countrynametv.setText(c.getCountryname());
+            populationtv.setText(c.getPopulation()+"");
+            return v;
 
 
         }
     }
+//    public class SongAdapter extends ArrayList<Song> {
+//
+//
+//        public class OurSongAdapter extends ArrayList<Song>{
+//
+//
+//                Context c;
+//                int resource;
+//                List<Song> objects;
+//                public OurSongAdapter(Context c1, int resource, List<Song> objects)
+//                {
+//                super(c1,resource,objects);
+//                this.c = c1;
+//                this.resource = resource;
+//                this.objects = objects;
+//            }
+//
+//
+//
+//
+//
+//        }
+//    }
 }
